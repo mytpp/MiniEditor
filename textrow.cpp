104b50099b981384881f9640f6ad97a11ae8e2ba
No98 <38655338+No98@users.noreply.github.com>
2018-06-08 01:05:18 +0800

    Update textrow.cpp

--- textrow.cpp
+++ textrow.cpp
@@ -1,5 +1,6 @@
 #include "textrow.h"
 #include "visitor\abstractvisitor.h"
+#include <iterator>
 
 
 
@@ -16,8 +17,19 @@ TextRow::TextRow(QString text)     //添加新内容到一行中
           row.push_back(text[i]);
 }
 
-QChar& TextRow::operator[](int position)
+ int TextRow::size() const
+ {
+     return row.size();
+ }
+
+ int TextRow::capacity() const
+ {
+     return row.capacity();
+ }
+
+QChar TextRow::operator[](int position)const
 {
+    qDebug()<<position;
     return row[position];
 }
 
@@ -26,23 +38,17 @@ bool TextRow::insert(int position, QChar cha)
     int current_capacity=row.capacity();
     int current_size = row.size();
     
-    int insert_position = position;
-    int i;
-    
-    if( position > current_capacity  ||  position > current_size )
-          return false;                       //considering the position is undesirable
+    if(position > current_size )
+          return false;                                       //considering the position is undesirable
    
-    if(position==current_capacity)
+    if(current_size==current_capacity)
          row.reserve(current_capacity+50);    //considering the space is not enough
-    
-    while(row[position]!='\n')
-         position++;
-    
-    for(i=position;i>=insert_position;i--)
-         row[position+1]=row[position];
-    
-    row[insert_position]=cha;
-    
+
+
+    auto i = row.begin();
+    advance(i,position);
+
+    row.insert(i,cha);
     return true;
 }
 
@@ -54,52 +60,69 @@ bool TextRow::insert(int position, QString str)
     if(length==0)
         return true;
     
-    if( position > current_capacity  ||  position > current_size )
-        return false;                           //considering the position is undesirable                        //considering oversteping the boundry
-    
+    if(  position > current_size )
+        return false;                           //considering the position is undesirable
+
     if( current_size+length >= current_capacity )
     {    
         int n=length/50;
         if( length%50!=0 )
-        {
               n = n+1;
-        }
         row.reserve(current_capacity+n*50);      //considering the space is not enough
     }
-    
-    QChar* data=str.data();
-    int i=0;
-    while(row[position]!='\n')
-    {     
-        row[position+length]=row[position];
-        row[position]=data[i];
-        i++;
-        position++;
-    }
-    row[position+length]='\n';
+
+    QChar* data=str.data();             //inserting the whole string one by one
+
+    auto j = row.begin();
+    advance(j,position);
+
+    for(int i=0;i<length;i++,j++)
+        row.insert(j,data[i]);
+
     return true;
 }
 
 
-bool TextRow::erase(int position)
+/*-------删除一行中position位置上的字符--------*/
+bool TextRow::erase(int position)     //position从第零位开始计
 {
-    int current_capacity=row.capacity();
-    int current_size = row.size();
-    
-    if( position > current_capacity  ||  position > current_size )
+    int current_size = row.size();    //是否考虑换行符也算一个字符？？
+
+    //considering the position is undesirable
+    if(  position > current_size - 1  )
         return false;
-    //considering the position is undesirable                        //considering oversteping the boundry
-    
-    
+
+    auto i = row.begin();
+    advance(i,position);
+    row.erase(i);
+    return true;
 }
 
+
+/*--------删除区间为[begin,end)，左闭右开--------*/
+/*begin和end直接表示下标 ，删除到end的前一位，*/
 bool TextRow::erase(int begin, int end)
 {
+    int current_size = row.size();
+
+    //considering the position is undesirable
+    if(  end > current_size   || begin < 0 )    //删除区间的开头不可以小于零，结尾不可以超过当前串的结尾
+        return false;
+
+    auto i = row.begin();
+    advance(i,begin);
+    auto j = row.begin();
+    advance(j,end);
+
+    row.erase(i,j);
     return true;
 
 }
 
 bool TextRow::traverse(AbstractVisitor &visitor)
 {
+    for(auto i=row.begin(); i!=row.end(); i++)
+        if(!visitor.visit(*i))
+                return false;
     return true;
 }
