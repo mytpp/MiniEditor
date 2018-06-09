f3c3df1578a58fcbb8ad86ab8933f96e7ad72ede
mytpp <yourtpp@outlook.com>
2018-06-09 16:38:13 +0800

    修复保存文件末尾换行符问题

--- Visitor/savevisitor.cpp
+++ Visitor/savevisitor.cpp
@@ -1,16 +1,27 @@
 #include "savevisitor.h"
 #include <QString>
+#include <QDebug>
 
 SaveVisitor::SaveVisitor(std::fstream &dest)
-    : to(dest)
+    : to(dest),
+      lineBreaking(false)
 {
 
 }
 
 bool SaveVisitor::visit(QChar& element)
 {
-    if(to << QString(element).toLocal8Bit().data())
-        return true;
-    else
-        return false;
+    /*delay inserting '\n'*/
+    if(lineBreaking)
+        to<<"\n";
+    if(element != '\n'){
+        if(to << QString(element).toLocal8Bit().data())
+            return true;
+        else
+            return false;
+        lineBreaking = false;
+    } else {
+        lineBreaking = true;
+    }
+    return true;
 }
