899595a9f70b6d9987d3272d1101eec2209a12f2
No98 <38655338+No98@users.noreply.github.com>
2018-06-09 12:02:53 +0800

    Update searchvisitor.cpp

--- Visitor/searchvisitor.cpp
+++ Visitor/searchvisitor.cpp
@@ -3,15 +3,56 @@
 SearchVisitor::SearchVisitor(QString format, Qt::CaseSensitivity cs)
     :target(format), sensitivity(cs), next(new int [format.size()])
 {
-
+    index=-1;
+    row_count = 0;
+    line_count= 0;
+    int i = 0;
+    next[0] = -1; //next[0]初始化为-1.  -1表示不存在相同的最大前缀和最大后缀
+    int j = -1;   //j初始化为-1
+    while(i < format.size())
+    {
+        if(j==-1 || format[i] == format[j] )
+        {
+            i++;
+            j++;
+            next[i]=j;
+        }
+        else j = next[j];
+    }
 }
+
 SearchVisitor::~SearchVisitor()
 {
     delete [] next;
 }
 
 bool SearchVisitor::visit(QChar& element)
-{
+{   
+    std::pair<int,int> position(0,0);
+    if(element!='\n' && index < target.size())
+    {
+       line_count++;
+       while(index>-1 && element!=target[index+1])
+           index=next[index];
+       if( element == target [index+1]  || index == -1 )
+           index++;
+       if (index == target.size()-1)
+       {
+           position.second = line_count-target.size();
+           position.first = row_count;
+           result.push_back(position);
+           index = -1;
+        }
+       return true;
+    }
+
+    else if(element=='\n')
+    {
+        row_count++;
+        index=-1;
+        return true;
+    }
+
     return false;
 }
 
@@ -22,6 +63,7 @@ const QString& SearchVisitor::getFormat() const
 
 std::vector<std::pair<int,int>>& SearchVisitor::getResult()
 {
+
     return result;
 }
 
