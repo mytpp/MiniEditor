1be9a52d380493f39601d758494fc01bd1acc691
No98 <38655338+No98@users.noreply.github.com>
2018-06-10 09:26:43 +0800

    少量注释

--- textrow.cpp
+++ textrow.cpp
@@ -3,45 +3,51 @@
 #include <iterator>
 
 
-
+/*构造函数*/
+/*为新行插入一个换行符，且预留50个字符的空间*/
 TextRow::TextRow()
 {
     row.push_back(QChar('\n'));
     row.reserve(50);
 }
 
-
-TextRow::TextRow(QString text)     //添加新内容到一行中
+/*构造函数*/
+/*添加新内容到一行中*/
+TextRow::TextRow(QString text)
 {
-    row.reserve((text.size() / 50 + 1) * 50);//实际长度多一个换行符
+    row.reserve((text.size() / 50 + 1) * 50); //实际长度多一个换行符
 
     for(int i=0;i<text.size();i++)
         row.push_back(text[i]);
-    row.emplace_back('\n');//保证换行符始终在最后
+    row.emplace_back('\n'); //保证换行符始终在最后
 }
 
+/*返回该行的长度*/
  int TextRow::size() const
  {
      return row.size();
  }
 
+ /*返回该行的容量*/
  int TextRow::capacity() const
  {
      return row.capacity();
  }
 
+ /*运算符重载*/
 QChar TextRow::operator[](int position)const
 {
     return row[position];
 }
 
+/*插入字符*/
 bool TextRow::insert(int position, QChar cha)
 {
     int current_capacity=row.capacity();
     int current_size = row.size();
     
     if(position > current_size )
-          return false;                                       //considering the position is undesirable
+          return false;                 //considering the position is undesirable
    
     if(current_size==current_capacity)
          row.reserve(current_capacity+50);    //considering the space is not enough
@@ -54,6 +60,8 @@ bool TextRow::insert(int position, QChar cha)
     return true;
 }
 
+
+/*插入字符串*/
 bool TextRow::insert(int position, QString str)
 {
     int length = str.size();
@@ -92,7 +100,7 @@ bool TextRow::insert(int position, QString str)
 }
 
 
-/*-------删除一行中position位置上的字符--------*/
+/*删除一行中position位置上的字符*/
 bool TextRow::erase(int position)     //position从第零位开始计
 {
     int current_size = row.size();    //是否考虑换行符也算一个字符？？
@@ -108,7 +116,7 @@ bool TextRow::erase(int position)     //position从第零位开始计
 }
 
 
-/*--------删除区间为[begin,end)，左闭右开--------*/
+/*删除区间为[begin,end)，左闭右开*/
 /*begin和end直接表示下标 ，删除到end的前一位，*/
 bool TextRow::erase(int begin, int end)
 {
@@ -128,6 +136,7 @@ bool TextRow::erase(int begin, int end)
     return true;
 }
 
+/*遍历函数*/
 bool TextRow::traverse(AbstractVisitor &visitor)
 {
     for(auto i=row.begin(); i!=row.end(); i++)
